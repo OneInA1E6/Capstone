62695cad488394fc9fee55c672becf225f7c554d
GMcLachlan45 <gmclachlan45@gmail.com>
2022-10-20 10:57:58 -0700

    scaffold the seeders

---
+++ src/database/seeders/GroupSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class GroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert();
+    }
+}
