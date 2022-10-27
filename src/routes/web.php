b7f23f14311c6235c37b20143933cd5b007f4295
GMcLachlan45 <gmclachlan45@gmail.com>
2022-10-27 13:40:49 -0700

    scaffold backend for routing to page

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\Http\Controllers;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,5 +29,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/groups', [Controllers\GroupController::class, 'show'])
+    ->middleware(['auth', 'verified'])->name('groups');
 
 require __DIR__.'/auth.php';
