0bda6ddbefbe60e6d3a83bce4d56f6f35cf79277
GMcLachlan45 <gmclachlan45@gmail.com>
2022-10-27 13:41:14 -0700

    Add the index file to render

---
+++ src/resources/js/Pages/Group/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <Layout>
+        I am very cool
+        <pre>
+            {{props.groups}}
+        </pre>
+
+    </Layout>
+
+</template>
+
+<script setup>
+import Layout from '@/Layouts/AppLayout.vue'
+
+const props = defineProps({
+    groups: Object
+})
+</script>
