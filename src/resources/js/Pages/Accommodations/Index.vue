307542ff12c5530621e0729588f7d2fcbe1d0ebf
mvallido <vallido.michael@gmail.com>
2022-11-04 11:37:35 -0700

    Fixing Accommodations Index

--- src/resources/js/Pages/Accommodations/Index.vue
+++ src/resources/js/Pages/Accommodations/Index.vue
@@ -1,4 +1,5 @@
 <template>
+    
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
@@ -12,25 +13,40 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        Accommodations Table
+                        Accommodations Tables testing 123 Does this work now
                     </div>
                 </div>
             </div>
         </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        {{props.accNumRooms}}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <pre>
+            {{props.accommodationsAll}}
+        </pre>
     </AuthenticatedLayout>
 </template>
 
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import SearchBar from '@/Components/SearchBar.vue'
-import { Head } from '@inertiajs/inertia-vue3';
-
-import { ref } from "vue";
-let input = ref("");
-const fruits = ["apple", "banana", "orange"];
-function filteredList() {
-  return fruits.filter((fruit) =>
-    fruit.toLowerCase().includes(input.value.toLowerCase())
-  );
-}
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import SearchBar from '@/Components/SearchBar.vue'
+    import { Head } from '@inertiajs/inertia-vue3';
+    import { ref } from "vue";
+    let input = ref("");
+    const fruits = ["apple", "banana", "orange"];
+    function filteredList() {
+        return fruits.filter((fruit) =>
+            fruit.toLowerCase().includes(input.value.toLowerCase())
+    );
+    }
+    const props = defineProps({
+        accommodationsAll: Object,
+        accNumRooms: Object
+    })
 </script>
