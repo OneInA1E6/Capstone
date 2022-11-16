890bed01c9af0b095e4c1fb4426e38b99414f16a
KentonMewling <tjwright1228@gmail.com>
2022-11-16 17:44:01 +0000

    fixed naming issue

--- src/resources/js/Pages/Bookings/Index.vue
+++ src/resources/js/Pages/Bookings/Index.vue
@@ -1,76 +1,32 @@
 <template>
-    <AppLayout>
+    <AuthenticatedLayout>
         <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                Bookings
+                bookings
             </h2>
         </template>
 
-        
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        <form @submit.prevent="submit">
-                          <div>
-                              <InputLabel for="groupId" value="Group Id" />
-                              <TextInput id="groupId" type="text" class="mt-1 block w-full" v-model="form.groupId"/>
-                              <InputError class="mt-2" :message="form.groupId" />
-                          </div>
-                          <div>
-                              <InputLabel for="duration" value="Duration of Stay" />
-                              <TextInput id="duration" type="text" class="mt-1 block w-full" v-model="form.duration"/>
-                              <InputError class="mt-2" :message="form.duration" />
-                          </div>
-
-                          <div class="flex justify-end mt-4">
-                              <Button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                                  Submit
-                              </Button>
-                          </div>
-                        </form>
-                      </div>
-                  </div>
-            </div>
-        </div>
-
-
+        I am very cool
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <pre>
-                          {{props.booking}}
+                            {{props.bookings}}
                         </pre>
-                      </div>
-                  </div>
+                    </div>
+                </div>
             </div>
         </div>
-          
-                      
 
-
-    </AppLayout>
+    </AuthenticatedLayout>
 
 </template>
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import Button from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { useForm } from '@inertiajs/inertia-vue3';
+
 const props = defineProps({
-    booking: Object,
+    bookings: Object
 })
-const form = useForm({
-    groupId: '',
-    duration: '',
-});
-const submit = () => {
-    form.post(route('bookings.create'), {
-        onFinish: () => form.reset(),
-    })
-  }
 </script>
