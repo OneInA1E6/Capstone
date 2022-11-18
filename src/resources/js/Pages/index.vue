f8060ad2444777de5547786d5c565138e979f125
KentonMewling <tjwright1228@gmail.com>
2022-11-18 17:53:32 +0000

    buttons now work bopys:

--- src/resources/js/Pages/index.vue
+++ src/resources/js/Pages/index.vue
@@ -5,19 +5,18 @@
     
         <span>
             <img src="..\..\images\fire-burning-down-a-building.jpg" class = "justify-center items-center relative w-screen h-screen flex">
-                <div class = " flex justify-center items-center">
-                    <img src="..\..\images\GH-logo.jpg" class=" w-1/3 h-2/4 justify-center object-center absolute top-0 p-10">
+                <div class = " flex justify-center items-center object-center">
+                    <img src="..\..\images\GH-logo.jpg" class=" w-1/3 h-2/4 justify-center items-center object-center absolute top-0 p-10">
                     
-                    <div v-if="canLogin" class="hidden">
-                        <button v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</button>
+                        <div v-if="canLogin" class = " items-center justify-center object-center absolute top-0 p-[420px] flex ">
+                            <NavLink v-if="$page.props.auth.user" :href="route('dashboard')" class="justify-center text-white flex w-40 h-10 my-2 font-normal bg-orange-300 rounded-full hover:bg-orange-350 drop-shadow-md " :as="button">Dashboard</NavLink>
 
-                        <template v-else>
-                            <button :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500">Log in</button>
-
-                            <button v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</button>
-                        </template>
+                            <div v-else  class = " items-center justify-center object-center flex absolute p-[420px] py-10 px-10">
+                                <NavLink v-if="canRegister" :href="route('register')" class="justify-center text-white  flex w-40 h-10 my-2 font-normal bg-orange-300 rounded-full hover:bg-orange-350 drop-shadow-md " :as="button">Register</NavLink>
+                                <NavLink :href="route('login')" class="justify-center text-white flex w-40 h-10 my-2 font-normal bg-orange-300 rounded-full hover:bg-orange-350 drop-shadow-md " :as="button">Log in</NavLink>
+                            </div>
+                        </div>
                     </div>
-                </div> 
         </span>
   
   
@@ -32,6 +31,9 @@
 
 <script setup>
 import { Head, Link } from '@inertiajs/inertia-vue3';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import Button from '@/Components/Button.vue'
 
 defineProps({
     canLogin: Boolean,
